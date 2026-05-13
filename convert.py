c398d7a104c0a68fb017348938597fe5f4b77a25
Jason Pollock <jason@pollock.ca>
2026-05-13 21:24:38 +1200

    Change convert to prune the dataset to N rows.
    It was taking way to long (10+hrs) to eval the dataset.
    When testing, use a small number of samples.

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import math
+import sys
 
 import pandas as pd
 import re
@@ -11,10 +12,16 @@ train_file = 'data/gms8k/train.json'
 validation_file = 'data/gms8k/validation.json'
 enable_debug = False
 split = True
+max_rows = sys.maxsize
 exec(open('configurator.py').read()) # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
 def write_dataframe(dataframe=None, file=None):
+    # debug(f"first row: {dataframe.at[0, 'question']} => {dataframe.at[0, 'answer']}, {dataframe.at[0, 'expected_value']}")
+    column_rename = {
+        "question": "prompt",
+        "answer": "response"
+    }
     train_out = dataframe.rename(columns=column_rename).to_json(orient='records', index=False, indent=4)
     with open(file, "w") as outfile:
         outfile.write(train_out)
@@ -28,16 +35,10 @@ dataframe["expected_value"] = dataframe['answer'].str.extract(r'#### (\d+)')
 dataframe["answer"] = dataframe['answer'].str.replace(r'(#### \d+)', '', regex=True,  flags=re.MULTILINE)
 
 # Modify every answer to convert '###.*$' to another column - true_value
-
-# debug(f"first row: {dataframe.at[0, 'question']} => {dataframe.at[0, 'answer']}, {dataframe.at[0, 'expected_value']}")
-column_rename = {
-    "question": "prompt",
-    "answer": "response"
-}
-
-row_split = None
+num_rows = min(max_rows, len(dataframe.index))
+debug(f"num_rows = {num_rows}")
+row_split = num_rows
 if split:
-    num_rows = len(dataframe.index)
     split_percent = 0.9
     row_split = math.trunc(num_rows * 0.9)
 
@@ -45,5 +46,5 @@ train_dataframe = dataframe[:row_split]
 validation_dataframe = dataframe[row_split:]
 
 write_dataframe(train_dataframe, train_file)
-if row_split is not None:
+if split:
     write_dataframe(validation_dataframe, validation_file)
